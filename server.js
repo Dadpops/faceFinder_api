60068ffd40566d0ddfc46889fe83635351a6dd35
Chris Cabena <chris.cabena65@gmail.com>
2020-12-04 15:31:15 -0700

    Added database connection

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-vertical-57818',
-    user : 'postgres',
-    password : 'test',
-    database : 'facefinder'
+    host : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
